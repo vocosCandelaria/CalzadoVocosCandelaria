8976e14097264d753b26fd0d3ec176f878be652b
Sofia Vocos <vocosdomenesofia@gmail.com>
2022-04-19 01:21:22 -0300

    navBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <NavBar/>
     </div>
   );
 }

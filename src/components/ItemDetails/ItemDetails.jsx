6750b74466d82fdb38669c7f46978e4ccfa2fd7c
vocosCandelaria <vocoscandelaria@gmail.com>
2022-05-09 23:37:43 -0300

    diseño responsive

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -4,17 +4,18 @@ import './ItemDetails.css'
 const ItemDetails = ({ detalleSillon }) => {
 
     return (
-        <div className="conteinerfluid m-5 d-flex">
-            <img src={detalleSillon.imagen} className="imagen" alt={detalleSillon.alt} />
-            <div className="bodyText m-5">
-                <h5 className="card-title">{detalleSillon.nombre}</h5>
-                <p className="card-text text-start">{detalleSillon.descripcion}</p>
-                <p className="card-text text-start">{detalleSillon.descripcion2}</p>
-                <p className="card-text text-start">{detalleSillon.tapizado}</p>
-                <p className="card-text text-start tiempo">{detalleSillon.tiempo}</p>
-                <ItemCount/>
+        <div className="container-fluid">
+            <div className="row contenedorCard">
+                <img className="imagen col-lg-6 col-md-6 col-sm-12" src={detalleSillon.imagen} alt={detalleSillon.alt} />
+                <div className="bodyText col-lg-6 col-md-6 col-sm-12">
+                    <h5 className="card-title">{detalleSillon.nombre}</h5>
+                    <p className="card-text text-start">{detalleSillon.descripcion}</p>
+                    <p className="card-text text-start">{detalleSillon.descripcion2}</p>
+                    <p className="card-text text-start">{detalleSillon.tapizado}</p>
+                    <p className="card-text text-start tiempo">{detalleSillon.tiempo}</p>
+                    <ItemCount />
+                </div>
             </div>
-            
         </div>
     )
 }
